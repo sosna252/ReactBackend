4b70debc1f7df0be062a26585d9b3330c401a8e3
krupinskij <jk@op.pl>
2020-01-08 21:20:18 +0100

    Initialize SpringBoot

---
+++ flatly-backend/src/main/java/pw/react/flatly/flatlybackend/FlatlyBackendApplication.java
@@ -0,0 +1,13 @@
+package pw.react.flatly.flatlybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatlyBackendApplication.class, args);
+	}
+
+}
---
+++ flatly-backend/src/test/java/pw/react/flatly/flatlybackend/FlatlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.react.flatly.flatlybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
